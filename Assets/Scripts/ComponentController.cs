4a9309767b2a42ceb3c29f533970ff8974593b93
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2024-01-04 14:52:00 +0300

    feat: Created and applied vector examples

--- Assets/Scripts/ComponentController.cs
+++ Assets/Scripts/ComponentController.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class ComponentController : MonoBehaviour
 {
-    [SerializeField] private GameObject cube;
+    [SerializeField] private Light light;
+    private Vector2 _firstVector, _secondVector, _finalVector;
+    
     void Start()
     {
-        cube.GetComponent<Light>().intensity = cube.GetComponent<Variable>().intensityValue;
-        cube.GetComponent<Variable>().ShowTheIntensityValue();
+        // Debug.Log(GetComponent<BoxCollider>().bounds.max);
+        // Debug.Log(GetComponent<BoxCollider>().bounds.min);
+        
+        light.intensity = 3.0f;
+        _firstVector = new Vector2(5.0f, 6.0f);
+        _secondVector = new Vector2(3.0f, 6.0f);
+        _finalVector = 2 * (_firstVector + _secondVector);
+        Debug.Log(_finalVector);
     }
 }
